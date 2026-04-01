4783a29951fb28deb18535ba49d6df3c2b7c279b
zzcak2100 <100412082@stud.sju.edu.tw>
2026-04-01 15:41:42 +0800

    Read Render PORT env var in server entrypoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
+import 'dotenv/config'
 import { app } from './app.js'
 
-const port = Number(process.env.API_PORT ?? 3001)
+const port = Number(process.env.PORT ?? process.env.API_PORT ?? 3001)
 
 app.listen(port, () => {
   console.log(`TOEIC vocabulary API listening on http://localhost:${port}`)
